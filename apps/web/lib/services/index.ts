d8ab14cb7732d4091715b8a5eaef2430c563055b
folajindayo <temidayofolajin@yahoo.com>
2025-11-13 01:03:57 +0100

    refactor(services): create service layer index
    - Centralize service exports
    - Provide single import point for services

---
+++ apps/web/lib/services/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Service layer exports
+ * Centralize all business logic services
+ */
+
+export * from './airdrop-check.service';
+export * from './portfolio.service';
+export * from './gas-tracker.service';
+export * from './risk-analysis.service';
+
