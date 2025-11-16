5094341f0dc02c26d1e72c2d02a5ac7660661c7d
folajindayo <temidayofolajin@yahoo.com>
2025-11-16 21:21:37 +0100

    Create core/utils/index.ts with organized exports

---
+++ apps/web/src/core/utils/index.ts
@@ -0,0 +1,36 @@
+/**
+ * Core utilities barrel export
+ * Centralized export for all core utility functions
+ * @module core/utils
+ */
+
+// Array utilities
+export * from './array';
+
+// Date/time utilities
+export * from './datetime';
+
+// Validation utilities
+export * from './validation';
+
+// Formatting utilities
+export * from './formatting';
+
+// Number utilities
+export * from './number';
+
+// String utilities
+export * from './string';
+
+// Object utilities
+export * from './object';
+
+// URL utilities
+export * from './url';
+
+// Hash utilities
+export * from './hash';
+
+// Retry utilities
+export * from './retry';
+
