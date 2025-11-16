fb8323f8342781fb8d72b721dce2e440275f3b48
seyi.idowu <shezi357412@icloud.com>
2025-11-17 00:19:00 +0100

    Add export file for latest onchain features

---
+++ apps/web/lib/onchain/exports-new-features.ts
@@ -0,0 +1,15 @@
+/**
+ * Export file for latest onchain features (768-797)
+ * Centralized exports for easy importing
+ */
+
+export * from './types-new-features';
+export * from './utils-new-features';
+export * from './helpers-new-features';
+export * from './validation-new-features';
+export * from './errors-new-features';
+
+export const LATEST_FEATURES_COUNT = 30;
+export const LATEST_FEATURES_RANGE = '768-797';
+export const TOTAL_FEATURES = 830;
+
